6c46eb58b0d76a4f56c50dfa8d1c118c53e34818
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2012-09-13 12:33:22 +0200

    fix bug in clean_events command

--- management/commands/clean_events.py
+++ management/commands/clean_events.py
@@ -2,8 +2,8 @@ from datetime import datetime, timedelta
 
 from django.core.management.base import NoArgsCommand, CommandError
 
-#from machiavelli import models
 from condottieri_events import models
+import machiavelli.models as machiavelli
 
 AGE=30*24*60*60
 
@@ -17,7 +17,7 @@ This script deletes all events in finished games that are older than AGE days.
 		age = timedelta(0, AGE)
 		threshold = datetime.now() - age
 		print "Deleting events that were added before %s" % threshold
-		old_events = models.BaseEvent.objects.filter(game__phase__exact=models.PHINACTIVE,
+		old_events = models.BaseEvent.objects.filter(game__phase__exact=machiavelli.PHINACTIVE,
 									game__slots__exact=0,
 									game__last_phase_change__lt=threshold)
 		print "%s events will be deleted" % len(old_events)
